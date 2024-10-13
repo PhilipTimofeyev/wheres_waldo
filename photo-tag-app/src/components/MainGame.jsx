d21629e16efc5cd0d88ecc2e4c684b3b9dfde49a
Philip Timofeyev <philiptimofeyev@gmail.com>
2024-10-13 14:42:46 -0700

    Set up active storage and rails API

--- photo-tag-app/src/components/MainGame.jsx
+++ photo-tag-app/src/components/MainGame.jsx
@@ -1,6 +1,30 @@
-import { React, useState } from 'react'
+import { React, useState, useEffect } from 'react'
 import Dropdown from './Dropdown';
 import styles from './MainGame.module.css'
+
+const API_URL = "http://127.0.0.1:3000/api/pictures"
+
+function getAPIData() {
+    const [data, setData] = useState();
+
+    useEffect(() => {
+        // fetch data
+        const dataFetch = async () => {
+            const data = await (
+                await fetch(
+                    API_URL,
+                )
+            ).json();
+
+            // set state when the data received
+            setData(data);
+        };
+
+        dataFetch();
+    }, []);
+
+        
+}
 
 function MainGame() {
 
@@ -8,6 +32,23 @@ function MainGame() {
     const [showDropdown, setShowDropdown] = useState(false); 
     const [showCircle, setShowCircle] = useState(false); 
     const [screenSize, setScreenSize] = useState(0)
+    const [data, setData] = useState();
+
+    useEffect(() => {
+        // fetch data
+        const dataFetch = async () => {
+            const data = await (
+                await fetch(
+                    API_URL,
+                )
+            ).json();
+
+            // set state when the data received
+            setData(data);
+        };
+
+        dataFetch();
+    }, []);
 
     const characters = [
         {id: 1, name:'Waldo'},
@@ -25,12 +66,14 @@ function MainGame() {
         setShowDropdown(true)
     }
 
+    // getAPIData()
+
   return (
     <div>
         <img className={styles.gameImage} onClick={handleClick}
             src="./src/assets/images/wheres-waldo-beach.jpg"
         />
-
+          <div>{data && <h1>{data[0].title}</h1>}</div>
         {showDropdown &&
               <div className={styles.targetSquare} style={{ left: mouseCoord[0], top: mouseCoord[1], width: screenSize, height: screenSize }}><Dropdown characters={characters}/></div>}
         
