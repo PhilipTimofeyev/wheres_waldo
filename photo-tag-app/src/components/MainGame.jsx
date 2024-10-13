05715b7af79a232a6cd9908b890b7261cd8a3909
Philip Timofeyev <philiptimofeyev@gmail.com>
2024-10-13 09:50:56 -0700

    Add CSS for main game

--- photo-tag-app/src/components/MainGame.jsx
+++ photo-tag-app/src/components/MainGame.jsx
@@ -1,23 +1,31 @@
 import { React, useState } from 'react'
+import styles from './MainGame.module.css'
 
 function MainGame() {
 
     const [mouseCoord, setMouseCoord] = useState()
+    const [showCircle, setShowCircle] = useState(false); 
 
     function handleClick(e) {
         const coordX = e.clientX
         const coordY = e.clientY
 
         setMouseCoord([coordX, coordY])
+        setShowCircle(true)
 
-        console.log([coordX, coordY])
+        // console.log([coordX, coordY])
     }
 
   return (
     <div>
-          <img onClick={handleClick}
+          <img className={styles.gameImage} onClick={handleClick}
               src="./src/assets/images/wheres-waldo-beach.jpg"
           />
+
+          {/* {console.log(mouseCoord[0])} */}
+
+          {showCircle && <div className={styles.circle} style={{ left: mouseCoord[0], top: mouseCoord[1] }}></div>}
+
     </div>
   )
 }
