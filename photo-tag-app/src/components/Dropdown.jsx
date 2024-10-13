9d2c3f49ccb3e54de5f7b7491ad633faec4ec3f0
Philip Timofeyev <philiptimofeyev@gmail.com>
2024-10-13 13:00:41 -0700

    Add dropdown component

---
+++ photo-tag-app/src/components/Dropdown.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import styles from './Dropdown.module.css'
+
+function Dropdown(props) {
+
+    function handleClick(e) {
+        console.log(e.target.innerText)
+    }
+
+    const characterList = props.characters.map(character => 
+        <li onClick={handleClick} key={character.id} className={styles.character}>{character.name}</li>
+    )
+
+    return (
+        <div className={styles.dropdown}>
+            <ul className={styles.characterList}>
+            {characterList}
+        </ul>
+        </div>
+    )
+}
+
+export default Dropdown
--- photo-tag-app/src/components/MainGame.jsx
+++ photo-tag-app/src/components/MainGame.jsx
@@ -1,11 +1,19 @@
 import { React, useState } from 'react'
+import Dropdown from './Dropdown';
 import styles from './MainGame.module.css'
 
 function MainGame() {
 
     const [mouseCoord, setMouseCoord] = useState()
+    const [showDropdown, setShowDropdown] = useState(false); 
     const [showCircle, setShowCircle] = useState(false); 
     const [screenSize, setScreenSize] = useState(0)
+
+    const characters = [
+        {id: 1, name:'Waldo'},
+        {id: 2, name:'Bertha'},
+        {id: 3, name: 'Rick'}
+    ]
 
     function handleClick(e) {
         const coordX = e.clientX
@@ -14,17 +22,19 @@ function MainGame() {
         setMouseCoord([coordX, coordY])
         setScreenSize(e.view.parent.innerWidth * .015)
         setShowCircle(true)
+        setShowDropdown(true)
     }
 
   return (
     <div>
-          <img className={styles.gameImage} onClick={handleClick}
-              src="./src/assets/images/wheres-waldo-beach.jpg"
-          />
+        <img className={styles.gameImage} onClick={handleClick}
+            src="./src/assets/images/wheres-waldo-beach.jpg"
+        />
 
-          {/* {console.log(mouseCoord[0])} */}
-
-          {showCircle && <div className={styles.targetSquare} style={{ left: mouseCoord[0], top: mouseCoord[1], width: screenSize, height: screenSize }}></div>}
+        {showDropdown &&
+              <div className={styles.targetSquare} style={{ left: mouseCoord[0], top: mouseCoord[1], width: screenSize, height: screenSize }}><Dropdown characters={characters}/></div>}
+        
+        {showCircle && <div className={styles.targetSquare} style={{ left: mouseCoord[0], top: mouseCoord[1], width: screenSize, height: screenSize }}></div>}
 
     </div>
   )
