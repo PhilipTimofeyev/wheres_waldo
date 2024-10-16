825ca6560c8ea0fa29fc023af3841f52db6b73d2
Philip Timofeyev <philiptimofeyev@gmail.com>
2024-10-16 13:06:12 -0700

    Add post and patch API call

--- photo-tag-app/src/components/Score.jsx
+++ photo-tag-app/src/components/Score.jsx
@@ -3,7 +3,9 @@ import { React, useState, useEffect } from 'react'
 
 function Score({startGame}) {
     const [data, setData] = useState(null);
+    const [scoreID, setScoreID] = useState()
     const API_URL = "http://127.0.0.1:3000/api/scores"
+    const UPDATE_URL = "http://127.0.0.1:3000/api/scores/1"
     
     const postBody = {
         username: 'test score',
@@ -28,16 +30,43 @@ function Score({startGame}) {
             }
             const responseData = await response.json();
             setData(responseData);
+            setScoreID(responseData.id)
         } catch (error) {
             console.error('Error:', error);
+        }
     }
-}
+
+    const updateBody = {
+        username: 'update score',
+    };
+
+    const updateScore = async (event) => {
+        console.log(scoreID)
+        const requestOptionsPatch = {
+            method: 'PATCH',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify(updateBody)
+        };
+
+        try {
+            const response = await fetch(`http://127.0.0.1:3000/api/scores/${scoreID}`, requestOptionsPatch);
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
+            }
+            const responseData = await response.json();
+            setData(responseData);
+        } catch (error) {
+            console.error('Error:', error);
+        }
+    }
+
   return (
     <div>
       <h1>SCORE</h1>
-        {/* {<button className='startBtn' onClick={getApi}>Hmm</button>} */}
+        {<button className='startBtn' onClick={updateScore}>Hmm</button>}
     </div>
   )
+
 }
 
 export default Score
