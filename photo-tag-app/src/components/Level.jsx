0ed97839a956055d36555b3a51030a6c674f4753
Philip Timofeyev <philiptimofeyev@gmail.com>
2024-10-17 10:45:45 -0700

    Create level component

---
+++ photo-tag-app/src/components/Level.jsx
@@ -0,0 +1,126 @@
+import { React, useState, useEffect, useRef } from 'react'
+import Dropdown from './Dropdown';
+import styles from './MainGame.module.css'
+
+const SQUARE_SIZE = .015
+
+function Level({level, setFound, found, setEndGame}) {
+    const [data, setData] = useState();
+    const [mouseCoord, setMouseCoord] = useState()
+    const [showDropdown, setShowDropdown] = useState(false); 
+    const bounds = useRef()
+
+    useEffect(() => {
+        const API_URL = `http://127.0.0.1:3000/api/pictures/${level.id}`
+        const dataFetch = async () => {
+            const data = await (
+                await fetch(
+                    API_URL,
+                )
+            ).json();
+
+            setData(data);
+        };
+        dataFetch();
+    }, []);
+
+    function handleClick(e) {
+        setMouseCoord([e.clientX, e.clientY])
+        showDropdown ? setShowDropdown(false) : setShowDropdown(true)
+        bounds.current = e.target.getBoundingClientRect()
+    }
+
+    function picture() {
+        return (
+            <img onClick={handleClick} className={styles.gameImage} src={data.picture.image} />
+        )
+    }
+
+    function dropdown() {
+        return (
+            <>
+                <div className={styles.dropdown} style={{ left: mouseCoord[0], top: mouseCoord[1] }}><Dropdown handleSelection={handleSelection} characters={data.characters} bounds={bounds.current} found={found} /></div>
+                <div className={styles.targetSquare} style={{ left: mouseCoord[0], top: mouseCoord[1], width: bounds.current.width * SQUARE_SIZE, height: bounds.current.width * SQUARE_SIZE, borderWidth: bounds.current.width * .003 }}></div>
+            </>
+        )
+    }
+
+    function normalizeCharCoord(character) {
+        // calculates ratio of character position relative to the image size
+        const x_perc = character.x_coord / data.picture.image_width
+        const y_perc = character.y_coord / data.picture.image_height
+
+        const x = bounds.current.width * x_perc
+        const y = bounds.current.height * y_perc
+
+        const charCoord = { charX: x, charY: y }
+
+        return charCoord
+    }
+
+    function normalizedUserCoord() {
+        // calculates the mouse position relative to the image
+        const x = mouseCoord[0] - bounds.current.left
+        const y = mouseCoord[1] - bounds.current.top
+
+        const userCoord = { userX: x, userY: y }
+
+        return userCoord
+    }
+
+    function handleSelection(e) {
+        const charName = e.target.innerText
+        const charObj = data.characters.find(char => char.name === charName)
+
+        const userCoord = normalizedUserCoord()
+        const charCoord = normalizeCharCoord(charObj)
+
+        // console.log(verifyCoord(userCoord, charCoord))
+        if (verifyCoord(userCoord, charCoord)) {
+            setFound([
+                ...found, { name: charObj.name, id: charObj.id, x_coord: mouseCoord[0], y_coord: mouseCoord[1] }
+            ])
+        }
+    }
+
+    function verifyCoord(userCoord, charCoord) {
+        const { userX, userY } = userCoord
+        const { charX, charY } = charCoord
+
+        const squareValidAreaX = (bounds.current.width * SQUARE_SIZE / 2)
+        const squareValidAreaY = (bounds.current.width * SQUARE_SIZE / 2) + 10
+
+        return (userX > (charX - squareValidAreaX) && userX < (charX + squareValidAreaX)) &&
+            (userY > (charY - squareValidAreaY) && userY < (charY + squareValidAreaY))
+    }
+
+
+    useEffect(() => {
+        gameOver()
+    }, [found]);
+
+    function gameOver() {
+        let allFound
+        console.log(data)
+        if (data) {
+            allFound = data.characters.length === found.length
+        }
+        if (allFound) setEndGame(true)
+    }
+
+    const foundChars = found.map(char => {
+        return < div key={char.id} className={styles.foundSquare} style={{ left: char.x_coord, top: char.y_coord, width: bounds.current.width * SQUARE_SIZE, height: bounds.current.width * SQUARE_SIZE, borderWidth: bounds.current.width * .003 }}></div >
+    })
+    
+
+
+  return (
+    <div>
+        {data && picture()}
+        {showDropdown && dropdown()}
+        {found && foundChars}
+    </div>
+  )
+}
+
+export default Level
--- photo-tag-app/src/components/MainGame.jsx
+++ photo-tag-app/src/components/MainGame.jsx
@@ -1,138 +1,24 @@
 import { React, useState, useEffect, useRef } from 'react'
+import Level from './Level';
 import Timer from './Timer';
-import Dropdown from './Dropdown';
 import Score from './Score';
 import styles from './MainGame.module.css'
 
-const SQUARE_SIZE = .015
 
 function MainGame({startGame, level}) {
-    const [mouseCoord, setMouseCoord] = useState()
-    const [showDropdown, setShowDropdown] = useState(false); 
-    const [data, setData] = useState();
-    const [found, setFound] = useState([])
     const [finalTime, setFinalTime] = useState()
     const [endGame, setEndGame] = useState()
     const [startRound, setStartRound] = useState(false)
-    const bounds = useRef()
+    const [found, setFound] = useState([])
+    console.log(level)
 
-    useEffect(() => {
-        const API_URL = `http://127.0.0.1:3000/api/pictures/${level.id}`
-        const dataFetch = async () => {
-            const data = await (
-                await fetch(
-                    API_URL,
-                )
-            ).json();
-
-            setData(data);
-        };
-        dataFetch();
-    }, []);
-
-    function handleClick(e) {
-        setMouseCoord([e.clientX, e.clientY])
-        showDropdown ? setShowDropdown(false) : setShowDropdown(true)
-        bounds.current = e.target.getBoundingClientRect()
-    }
-
-    function normalizeCharCoord(character) {
-        // calculates ratio of character position relative to the image size
-        const x_perc = character.x_coord / data.picture.image_width
-        const y_perc = character.y_coord / data.picture.image_height
-
-        const x = bounds.current.width * x_perc
-        const y = bounds.current.height * y_perc
-
-        const charCoord = { charX: x, charY: y }
-
-        return charCoord
-    }
-
-    function normalizedUserCoord() {
-        // calculates the mouse position relative to the image
-        const x = mouseCoord[0] - bounds.current.left
-        const y = mouseCoord[1] - bounds.current.top
-
-        const userCoord = { userX: x, userY: y }
-
-        return userCoord
-    }
-
-
-    useEffect(() => {
-        gameOver()
-    }, [found]);
-
-    function handleSelection(e) {
-        const charName = e.target.innerText
-        const charObj = data.characters.find(char => char.name === charName)
-
-        const userCoord = normalizedUserCoord()
-        const charCoord = normalizeCharCoord(charObj)
-        
-        // console.log(verifyCoord(userCoord, charCoord))
-        if (verifyCoord(userCoord, charCoord)) {
-            setFound([
-                ...found, { name: charObj.name, id: charObj.id, x_coord: mouseCoord[0], y_coord: mouseCoord[1] }
-            ])
-        }
-    }
-
-    function gameOver() {
-        let allFound
-        if (data) {
-             allFound = data.characters.length === found.length
-        }
-        if (allFound) setEndGame(true)
-    }
-
-    function verifyCoord(userCoord, charCoord) {
-        const {userX, userY} = userCoord
-        const {charX, charY} = charCoord
-
-        const squareValidAreaX = (bounds.current.width * SQUARE_SIZE / 2)
-        const squareValidAreaY = (bounds.current.width * SQUARE_SIZE / 2) + 10
-
-        return (userX > (charX - squareValidAreaX) && userX < (charX + squareValidAreaX)) &&
-                (userY > (charY - squareValidAreaY) && userY < (charY + squareValidAreaY))
-    }
-    
-    function picture() {
-        return (
-            <img className={styles.gameImage} onClick={handleClick} src={data.picture.image} />
-        )
-    }
-
-    function dropdown() {
-        return (
-            <>
-            <div className={styles.dropdown} style={{ left: mouseCoord[0], top: mouseCoord[1] }}><Dropdown handleSelection={handleSelection} characters={data.characters} bounds={bounds.current} found={found} /></div>
-            <div className={styles.targetSquare} style={{ left: mouseCoord[0], top: mouseCoord[1], width: bounds.current.width * SQUARE_SIZE, height: bounds.current.width * SQUARE_SIZE, borderWidth: bounds.current.width * .003 }}></div>
-            </>
-        )
-    }
-
-    function squareMarker() {
-        return (
-            <div className={styles.targetSquare} style={{ left: mouseCoord[0], top: mouseCoord[1], width: bounds.current.width * SQUARE_SIZE, height: bounds.current.width * SQUARE_SIZE, borderWidth: bounds.current.width * .003 }}></div>
-        )
-    }
-
-    const foundChars = found.map(char => {
-        return < div key={char.id} className={styles.foundSquare} style={{ left: char.x_coord, top: char.y_coord, width: bounds.current.width * SQUARE_SIZE, height: bounds.current.width * SQUARE_SIZE, borderWidth: bounds.current.width * .003 }}></div >
-    })
 
 
   return (
     <div>
-          {/* {<button className='startBtn' onClick={dataFetch}>Start Round</button>} */}
         <Score startGame={startGame} endGame={endGame}/>
         {!finalTime && <Timer startGame={startGame} setFinalTime={setFinalTime} endGame={endGame} />}
-        {/* {finalTime && <h1>You did it in {finalTime} seconds!</h1>} */}
-        {data && picture()}
-        {showDropdown && dropdown()}
-        {found && foundChars}
+        <Level level={level} setFound={setFound} found={found} setEndGame={setEndGame}/>
     </div>
   )
 }
